4bf94b9c8c14a3e62a925c4afc89518a4d5fb97c
ManKiam <61895156+ManKiam@users.noreply.github.com>
2020-11-23 16:26:22 +0330

    Add files via upload

--- abc.bat
+++ abc.bat
@@ -1,1 +1,1 @@
-"C:\users\lenovo\appdata\roaming\microapp64\Access Manager Security.exe" > "C:\Users\lenovo\logs.txt"
+"C:\users\lenovo\appdata\roaming\microapp64\Access Manager Security.exe" >"C:\Users\lenovo\logs.txt" 2>&1
